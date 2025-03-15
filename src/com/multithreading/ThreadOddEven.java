de0d01d96317b1f8042893310127d2e013dbc19e
sparshkalra <52778582+sparshkalra@users.noreply.github.com>
2025-03-15 13:28:04 +0530

    Update ThreadOddEven.java
    Updated comparator and runnable with java8

--- src/com/multithreading/ThreadOddEven.java
+++ src/com/multithreading/ThreadOddEven.java
@@ -5,25 +5,39 @@ import java.util.concurrent.Callable;
 public class ThreadOddEven {
 	int i=1;
 	
+	
+	/*
+	 * employees.sort(Comparator.comparing(Employee::getSalary)
+                         .thenComparing(Employee::getName));
+	 */
 	public static void main(String[] args)
 	{
 		ThreadOddEven toe = new ThreadOddEven();
 		
-	
-		
-		Thread t1 = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
+		Runnable r1 = () ->
+		{
 			try {
 				toe.printOdd();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-				
-			}
-		});
+		};
+		
+		Runnable task = () -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(Thread.currentThread().getName() + " - Count: " + i);
+                try {
+                	toe.printOdd();
+                	} catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+		
+		
+		Thread t1 = new Thread(r1);
 		
 		Thread t2 = new Thread(new Runnable() {
 			
